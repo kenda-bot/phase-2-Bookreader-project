108097f47e5d0426fb4e72491940c10d9e4f6de0
Margaret Kenda <margaret.kenda@yahoo.com>
2022-10-05 15:56:05 +0300

    listbooks and style the body

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,10 +1,29 @@
 import React,{useEffect,useState} from 'react'
 import "../App";
-import{API_url} from "../API
-function Books() {
+import{API_url} from "../API";
+
+const Books=()=> {
+  const [books,setBooks] =useState([]);
+  
+  useEffect(()=>{
+    fetch("https://example-data.draftbit.com/books?_limit=10")
+    .then((response)=>response.json())
+    .then((data)=>setBooks(data))
+   },[]);
+
+
   return (
-    <div className='books'>Books</div>
-  )
-}
+    <div className='books'>
+      {books.map((book)=>(
+        <div key ={book.id}className="book-list">
+          <div><h2>{book.title}</h2></div>
+          <div><img src ={book.image_url} alt ="#"/></div>
+          <div>
+            <button>Add to My Reading List</button>
+            </div>
+    </div>
+  ))}
+  </div>
+  )}
 
 export default Books
